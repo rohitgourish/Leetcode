6d11f72a61a4bee0f890144014bb98af7ea46101
Rohit Kumar Pydisetti <42351342+rohitgourish@users.noreply.github.com>
2025-03-07 16:41:59 -0800

    Time: 14 ms (29.75%) | Memory: 45 MB (11.86%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,6 +1,11 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+
         HashMap<Character, Integer> map = new HashMap<>();
+        
         for(char ch: s.toCharArray()){
             map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
